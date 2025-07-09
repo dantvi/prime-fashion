7402a80845813ee9bc8796648d308895a21fc129
Daniel Tving <daniel.tving@gmail.com>
2025-07-09 02:09:33 +0200

    added global theme and typography styles

---
+++ client/src/styles/Theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    titleActive: '#000000', // Links & titles
+    body: '#333333', // Body text
+    label: '#555555', // Label text
+    placeholder: '#888888', // Input placeholders
+    line: '#E0CFBA', // Borders, dividers
+    inputBackground: '#F9F9F9', // Input backgrounds
+    background: '#F8F0E7', // General background
+    offWhite: '#FCFCFC', // Avoid darkmode strain
+    primary: '#A8715A', // Primary action color
+    secondary: '#DD8560', // Accent and secondary actions
+  },
+  fonts: {
+    base: "'Tenor Sans', sans-serif",
+  },
+};
+
+export default theme;
