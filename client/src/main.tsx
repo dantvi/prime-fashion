b6c7f8a99e0ebc9f9ced33de6177854cfc976294
Daniel Tving <daniel.tving@gmail.com>
2025-07-19 20:27:36 +0200

    removed styled-components setup and theme files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import { CartProvider } from './contexts/cart-context.tsx';
 import './index.css';
 import App from './App.tsx';
-import theme from './styles/Theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </ThemeProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
   </StrictMode>
 );
